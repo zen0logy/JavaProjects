f7e5adf9b084938ca92284dfbe7889a9e5f747f4
zen <bulletice2@gmail.com>
2024-09-29 00:35:47 +0200

    Update Lotto.java

--- Lotto.java
+++ Lotto.java
@@ -7,10 +7,10 @@ public class Lotto {
 
         lottoTip(6);
 
-        System.out.println("Play again? (Y/N)");
+        System.out.println("Play again? (ENTER)");
         String option = sc.nextLine();
 
-        while (option.equalsIgnoreCase("y")) {
+        while (option.equalsIgnoreCase("")) {
             lottoTip(6); // adjust attempts here! (increases win chance)
         }
     }
@@ -20,24 +20,38 @@ public class Lotto {
         Scanner sc = new Scanner(System.in);
 
         int correctGuess = 0;
-        int correctInput = 0;
+        int[] correctInput = new int[attemptLimit];
 
         int[] lottoNumbers = new int[attemptLimit];
         int[] attempts = new int[attemptLimit];
 
+        int[] inputs = new int[attemptLimit];
+        int[] randoms = new int[attemptLimit];
+
         for (int i = 1; i <= attemptLimit; i++) {
             System.out.println("Enter your " + i + ". number: ");
 
-            int input = sc.nextInt();
-            int random = (int) (Math.random() * 45) + 1; // adjust lotto-range here! (increases win chance)
+            int input = sc.nextInt(); // 2
+            int random = (int) (Math.random() * 5) + 1; // 1
 
-            lottoNumbers[i - 1] = random;
-            attempts[i - 1] = input;
+            inputs[i - 1] = input;
+            randoms[i - 1] = random;
 
-            if (input == random) {
-                correctGuess++;
-                correctInput = input;
+            lottoNumbers[i - 1] = random; // 1
+            attempts[i - 1] = input; // 2
+        }
+
+        for (int i = 0; i < randoms.length; i++) {
+            for (int j = 0; j < inputs.length; j++) {
+                if (inputs[j] == randoms[i]) {
+                    correctGuess += 1;
+                    correctInput[j] = inputs[j];
+                }
             }
+        }
+
+        if(Arrays.asList(correctInput).contains(0)) {
+            correctInput = correctInput[];
         }
 
         // Enhancements
@@ -49,7 +63,7 @@ public class Lotto {
 
         System.out.println("==================== GUESSES ===================");
 
-        System.out.println("Guesses: " + correctInput);
+        System.out.println("Guesses: " + Arrays.toString(correctInput));
         System.out.println(correctGuess + " / " + attemptLimit + " guessed correctly");
 
         System.out.println("====================== WIN =====================");
@@ -67,7 +81,7 @@ public class Lotto {
         if (correctGuess == 6) {
             System.out.println("!!- JACKPOT -!!");
             System.out.println("YOU WON € 100.000,-");
-        } else if (correctGuess == 1 || correctGuess == 2) {
+        } if (correctGuess == 1 || correctGuess == 2) {
             System.out.println("You lose");
         }
 
